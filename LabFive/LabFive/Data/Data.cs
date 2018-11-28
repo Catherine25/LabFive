533883e95fcf55698a281378017ad1daab27d658
Catherine <Catherine25@users.noreply.github.com>
2018-11-28 19:14:17 +0200

    Added logic

---
+++ LabFive/LabFive/Data/Data.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LabFive
+{
+    [Serializable]
+    public class Data
+    {
+        //data
+        public string Subject { get; set; }
+        public string Student { get; set; }
+        public int Mark { get; set; }
+
+        //constructors
+        public Data(string _sub, string _stu, int _m)
+        {
+            Subject = _sub;
+            Student = _stu;
+            Mark = _m;
+        }
+    }
+}
---
+++ LabFive/LabFive/Data/DataController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LabFive
+{
+    [Serializable]
+    static class DataController
+    {
+        //data
+        public static List<Data> _dataToShow;
+        private static List<Data> _data;
+
+        //constructors
+        static DataController()
+        {
+            _data = new List<Data>();
+            _dataToShow = new List<Data>();
+        }
+    }
+}
